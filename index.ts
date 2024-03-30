ce05bd54f4563322c863f3de2712f61d89edc189
Talha Shaikh <127993601+Talha-Shaikh06@users.noreply.github.com>
2024-03-31 01:27:33 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,40 @@
+#! /usr/bin/env/ node
+import inquirer from "inquirer";
+
+let TODO: string[] = [];
+let loop = true;
+
+
+while(loop){
+    const answer :{
+        TODO: string,
+        addmore: boolean}= await inquirer.prompt([
+        {
+            type: "input",
+            name: "Todo list",
+            message: "What you want to add in your to-do list? "
+        },
+        {
+            type: "confirm",
+            name: "addmore",
+            message: "Do you want to add more things in your to-do list? ",
+        }
+    ])
+    const {TODO, addmore} = answer;
+    console.log(answer)
+    loop = addmore
+    if(TODO){
+        TODO.push(TODO)
+    } else{
+        console.log("Kindly add valid input in your to-do list \n")
+    }
+}
+
+if(TODO.length > 0){
+    console.log("Your To-do list: \n")
+    TODO.forEach(todo => {
+        console.log(TODO)
+    });
+} else {
+    console.log("There is no To-do work found in your list!!!\n")
+}
